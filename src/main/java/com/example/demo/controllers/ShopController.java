521bfc95f22d64508878eeb7cfaeed8a00bc767f
Anton Popov <popovaowork@gmail.com>
2021-11-27 14:01:08 +0200

    added methods get all and add to ShopController

--- src/main/java/com/example/demo/controllers/ShopController.java
+++ src/main/java/com/example/demo/controllers/ShopController.java
@@ -1,4 +1,16 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public interface ShopController {
+    @GetMapping("/get/all")
+    List<Product> findAllProducts();
+
+    @PostMapping("/add/product")
+    Product save(@RequestBody Product product);
 }
