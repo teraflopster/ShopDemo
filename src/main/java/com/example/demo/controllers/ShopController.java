8c9e6e4a8a0aaac7984bcd58867c9b16ead70026
Anton Popov <popovaowork@gmail.com>
2021-12-04 22:30:50 +0200

    added methods in product controller

--- src/main/java/com/example/demo/controllers/ShopController.java
+++ src/main/java/com/example/demo/controllers/ShopController.java
@@ -2,7 +2,10 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Product;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @RestController
@@ -11,6 +14,16 @@ public interface ShopController {
     @GetMapping("/get/all")
     List<Product> findAllProducts();
 
+    @GetMapping("/get/by/id")
+    Product findProductById();
+
+    @GetMapping("/get/by/type")
+    List<Product> findProductByType();
+
     @PostMapping("/add/product")
-    Product save(@RequestBody Product product);
+    Product save(@RequestParam MultipartFile file, @RequestParam String name,
+                 @RequestParam int cost, @RequestParam String desc) throws IOException, NoSuchAlgorithmException;
+
+    @DeleteMapping("/delete/by/id")
+    void deleteProductById();
 }
