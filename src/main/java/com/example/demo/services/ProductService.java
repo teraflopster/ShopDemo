19a655b0249762a8ce026407d2654c1e3d5ec129
Anton <popovaowork@gmail.com>
2021-12-09 00:48:21 +0200

    Created interface Product Service for work with database

---
+++ src/main/java/com/example/demo/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Product;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public interface ProductService {
+    void saveProduct(Product product);
+
+    List<Product> findAllProducts();
+
+    Optional<Product> findProduct(long id);
+
+    List<Product> findStudentsByType(String type);
+
+    List<Product> findAllStudentByName(String name);
+
+    List<Product> findAllStudentByCost(long cost);
+
+    void updateProduct(Product product, long id);
+
+    void deleteProduct(Product product);
+}
