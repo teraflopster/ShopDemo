c09e359e471428b5279871383d34812382d79585
Anton <popovaowork@gmail.com>
2021-12-21 12:12:42 +0200

    added work with minio

--- src/main/java/com/example/demo/controllers/ShopController.java
+++ src/main/java/com/example/demo/controllers/ShopController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 
 import com.example.demo.models.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,20 +11,24 @@ import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public interface ShopController {
-    @GetMapping(    "/product")
-    List<Product> findAllProducts();
+
+    @GetMapping("/product")
+    String findAllProducts(Model model);
+
+    @GetMapping("/product/new")
+    String createProduct(Model model);
 
     @GetMapping("/product/{id}")
-    Optional<Product> findProductById(@PathVariable int id);
+    String findProductById(@PathVariable int id, Model model);
 
     @GetMapping("/product/type/{type}")
     List<Product> findProductByType(@PathVariable String type);
 
     @PostMapping("/product")
-    Product save(@RequestParam MultipartFile image, @RequestParam String name, @RequestParam String type
+    String save(@RequestParam MultipartFile image, @RequestParam String name, @RequestParam String type
                 ,@RequestParam String desc,  @RequestParam long cost) throws IOException, NoSuchAlgorithmException;
 
     @DeleteMapping("/product/")
--- src/main/java/com/example/demo/controllers/ShopControllerImpl.java
+++ src/main/java/com/example/demo/controllers/ShopControllerImpl.java
@@ -1,40 +1,79 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Comment;
 import com.example.demo.models.Product;
+import com.example.demo.services.CommentServiceImpl;
+import com.example.demo.services.MinioService;
+import com.example.demo.services.MinioServiceImpl;
 import com.example.demo.services.ProductServiceImpl;
+import io.minio.GetPresignedObjectUrlArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import io.minio.http.Method;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-@RestController
+@Slf4j
+@Controller
 public class ShopControllerImpl implements ShopController{
 
+    private final ProductServiceImpl productService;
 
+    private final CommentServiceImpl commentService;
 
-
-    private ProductServiceImpl productService;
+    private final MinioServiceImpl minioService;
 
     @Autowired
-    ShopControllerImpl(ProductServiceImpl productService) {
+    ShopControllerImpl(ProductServiceImpl productService, CommentServiceImpl commentService, MinioServiceImpl minioService) {
         this.productService = productService;
+        this.commentService = commentService;
+        this.minioService = minioService;
+    }
+
+
+
+    @Override
+    public String findAllProducts(Model model) {
+        List<Product> products = productService.findAllProducts();
+        for (Product product : products) {
+            product.setImage(minioService.getUrl(product.getImage()));
+        }
+        model.addAttribute("products", products.stream().toArray());
+        return "/products";
     }
 
     @Override
-    public List<Product> findAllProducts() {
-        return productService.findAllProducts();
+    public String createProduct(Model model) {
+        model.addAttribute("some", "this is somethings");
+        return "new";
     }
 
     @Override
-    public Optional<Product> findProductById(@PathVariable int id) {
-        return productService.findProduct(id);
+    public String findProductById(@PathVariable int id, Model model) {
+        Optional<Product> product = productService.findProduct(id);
+        product.ifPresent(value -> value.setImage(minioService.getUrl(value.getImage())));
+        List<Comment> comments = commentService.findAllByProductId(product.get().getId());
+        model.addAttribute("product", product.stream().toArray());
+        model.addAttribute("comments", comments.stream().toArray());
+        return "/product";
     }
 
     @Override
@@ -42,11 +81,13 @@ public class ShopControllerImpl implements ShopController{
         return productService.findStudentsByType(type);
     }
 
+
+    /* POST /product */
     @Override
-    public Product save(MultipartFile image, String name, String type, String desc, long cost) {
-        Product product = new Product("image", name, type,desc,cost);
+    public String save(MultipartFile image, String name, String type, String desc, long cost) {
+        Product product = new Product(minioService.saveImage(image), name, type,desc,cost);
         productService.saveProduct(product);
-        return product;
+        return "redirect:/";
     }
 
     @Override
