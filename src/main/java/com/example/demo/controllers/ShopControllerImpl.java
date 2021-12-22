042fa084baf0c45609a13c09e8e0d5c1749b5cd4
Anton <popovaowork@gmail.com>
2021-12-22 19:12:29 +0200

    added search product by type

--- src/main/java/com/example/demo/controllers/ShopControllerImpl.java
+++ src/main/java/com/example/demo/controllers/ShopControllerImpl.java
@@ -3,33 +3,18 @@ package com.example.demo.controllers;
 import com.example.demo.models.Comment;
 import com.example.demo.models.Product;
 import com.example.demo.services.CommentServiceImpl;
-import com.example.demo.services.MinioService;
 import com.example.demo.services.MinioServiceImpl;
 import com.example.demo.services.ProductServiceImpl;
-import io.minio.GetPresignedObjectUrlArgs;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.http.Method;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.Resource;
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
@@ -56,7 +41,7 @@ public class ShopControllerImpl implements ShopController{
         for (Product product : products) {
             product.setImage(minioService.getUrl(product.getImage()));
         }
-        model.addAttribute("products", products.stream().toArray());
+        model.addAttribute("products", products.toArray());
         return "/products";
     }
 
@@ -70,15 +55,20 @@ public class ShopControllerImpl implements ShopController{
     public String findProductById(@PathVariable int id, Model model) {
         Optional<Product> product = productService.findProduct(id);
         product.ifPresent(value -> value.setImage(minioService.getUrl(value.getImage())));
-        List<Comment> comments = commentService.findAllByProductId(product.get().getId());
+        List<Comment> comments = commentService.findAllByProductId(id);
         model.addAttribute("product", product.stream().toArray());
-        model.addAttribute("comments", comments.stream().toArray());
+        model.addAttribute("comments", comments.toArray());
         return "/product";
     }
 
     @Override
-    public List<Product> findProductByType(@PathVariable String type) {
-        return productService.findStudentsByType(type);
+    public String findProductByType(@PathVariable String type, Model model) {
+        List<Product> products = productService.findStudentsByType(type);
+        for (Product product : products) {
+            product.setImage(minioService.getUrl(product.getImage()));
+        }
+        model.addAttribute("products", products.toArray());
+        return "/products";
     }
 
 
