a1ede0c8b378194c688ff337a4989652f8fafde1
Anton <popovaowork@gmail.com>
2021-12-25 01:23:24 +0200

    created repository for work with user entity on mongodb

---
+++ src/main/java/com/example/demo/repositorys/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repositorys;
+
+import com.example.demo.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
