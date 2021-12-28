3b593e98bfec24b3145077b947af1216a274c918
Anton <popovaowork@gmail.com>
2021-12-28 17:43:27 +0200

    Added method getAllUser and getUserByEmail in User controller

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -3,7 +3,13 @@ package com.example.demo.services;
 import com.example.demo.models.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public interface UserService {
+    List<User> getAllUser();
     void saveUser(User user);
+
+    Optional<User> getUserByEmail(String email);
 }
